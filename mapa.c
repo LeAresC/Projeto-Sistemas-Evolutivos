eb6dfc6012fa9b6d59a992dcfde12e712e09dbc1
Pedro Rodrigues Luiz Ferreira <pedrorodlf@usp.br>
2025-10-24 14:22:03 -0300

    Create mapa.c

---
+++ mapa.c
@@ -0,0 +1,18 @@
+
+
+int** criarMapaZero(int ordem){ //cria um mapa que será matriz de ordem [ordem][ordem]
+    int** linhas = (int**)calloc(ordem, sizeof(int)); //aloca memoria para as linhas
+    for (int i = 0; i<ordem; i++){
+        linhas[i] = (int*)calloc(ordem, sizeof(int)); //aloca memoria para as colunas
+    }
+
+    return linhas; //retorna a matriz gerada
+}
+
+void randomizarMapa(int** mapa, int ordem){ //
+    for(int i = 0; i<ordem; i++){
+        for (int j = 0; j< ordem; j++){
+            mapa[i][j] = 
+        }
+    }
+}
